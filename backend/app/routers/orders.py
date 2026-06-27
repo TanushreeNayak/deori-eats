a813e450997bda273b8b59be3e9ec16b5190d067
TanushreeNayak <tanushreenayak785@gmail.com>
2026-06-27 18:52:42 +0530

    New add-on + changes

--- backend/app/routers/orders.py
+++ backend/app/routers/orders.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from fastapi import HTTPException
+
 from app.deps import get_db
 from app.models.order import Order
 from app.schemas.order import OrderCreate
@@ -10,15 +10,15 @@ router = APIRouter(
     tags=["Orders"]
 )
 
+
 @router.post("/")
-def create_order(
+def place_order(
     order: OrderCreate,
     db: Session = Depends(get_db)
 ):
-
     new_order = Order(
-        user_id=order.user_id,
         restaurant_id=order.restaurant_id,
+        user_id=order.user_id,
         menu_item_id=order.menu_item_id,
         quantity=order.quantity
     )
@@ -32,11 +32,41 @@ def create_order(
         "order_id": new_order.id
     }
 
+
 @router.get("/")
-def get_orders(
+def get_all_orders(
     db: Session = Depends(get_db)
 ):
     return db.query(Order).all()
+
+
+@router.get("/user/{user_id}")
+def get_user_orders(
+    user_id: int,
+    db: Session = Depends(get_db)
+):
+    orders = (
+        db.query(Order)
+        .filter(Order.user_id == user_id)
+        .all()
+    )
+
+    return orders
+
+
+@router.get("/restaurant/{restaurant_id}")
+def get_restaurant_orders(
+    restaurant_id: int,
+    db: Session = Depends(get_db)
+):
+    orders = (
+        db.query(Order)
+        .filter(Order.restaurant_id == restaurant_id)
+        .all()
+    )
+
+    return orders
+
 
 @router.put("/{order_id}/status")
 def update_order_status(
@@ -44,6 +74,7 @@ def update_order_status(
     status: str,
     db: Session = Depends(get_db)
 ):
+
     order = db.query(Order).filter(Order.id == order_id).first()
 
     if not order:
